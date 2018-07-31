e738bbb5df55db3a2052a6f130fd5aa1a92ba47e
Jonathan Hefner <jonathan@hefner.pro>
2018-07-30 19:16:36 -0500

    Implement accumulative pagination

--- lib/moar/controller.rb
+++ lib/moar/controller.rb
@@ -5,8 +5,14 @@ module Moar
 
     def moar(relation)
       page = [params[:page].to_i, 1].max
+
       limit = PAGE_SIZES[page - 1] || PAGE_SIZES.sum
       offset = PAGE_SIZES.take(page - 1).sum * [page - PAGE_SIZES.length, 1].max
+
+      if params[:page_acc].present?
+        limit = [limit + offset, PAGE_SIZES.sum].min
+        offset = page <= PAGE_SIZES.length ? 0 : offset
+      end
 
       relation.offset(offset).limit(limit)
     end
--- test/controller_test.rb
+++ test/controller_test.rb
@@ -13,31 +13,51 @@ class ControllerTest < ActionDispatch::IntegrationTest
 
   fixtures "posts"
 
-  def test_page_sizes
-    moar_increments = Moar::Controller::PAGE_SIZES
+  def test_incremental_page_sizes
     max_page_size = moar_increments.sum
 
-    set_sizes = (0..moar_increments.length).map{|n| moar_increments.take(n).sum }.
-      flat_map{|set_size| [set_size, set_size + 1] } + [Post.count]
-
-    set_sizes.each do |set_size|
+    each_set_size do |set_size|
       page_sizes = get_page_sizes(set_size)
 
-      page_sizes[0...-1].zip(moar_increments).each do |actual, expected|
-        assert_equal(expected || max_page_size, actual)
+      (0...page_sizes.length).each do |i|
+        expected = moar_increments[i] || max_page_size
+        assert_operator page_sizes[i], (i < page_sizes.length - 1 ? :== : :<=), expected
       end
-
-      max_last_size = moar_increments[page_sizes.length - 1] || max_page_size
-      assert_operator page_sizes.last, :<=, max_last_size
 
       assert_equal set_size, page_sizes.sum
     end
   end
 
+  def test_accumulative_page_sizes
+    each_set_size do |set_size|
+      page_sizes = get_page_sizes(set_size, accumulative: true)
+
+      (0...page_sizes.length).each do |i|
+        expected = [moar_increments.take(i + 1).sum, set_size].min
+        assert_operator page_sizes[i], (i < page_sizes.length - 1 ? :== : :<=), expected
+      end
+
+      assert_operator page_sizes.length, :>=, (set_size.zero? ? 0 : moar_increments.length)
+      assert_equal set_size, page_sizes.drop(moar_increments.length - 1).sum
+    end
+  end
+
   private
 
-  def get_page_sizes(set_size)
-    assert_operator set_size, :<=, Post.count # sanity check
+  def moar_increments
+    Moar::Controller::PAGE_SIZES
+  end
+
+  def each_set_size
+    set_sizes = (0..moar_increments.length).map{|n| moar_increments.take(n).sum }.
+      flat_map{|set_size| [set_size, set_size + 1] } + [Post.count]
+
+    assert_operator set_sizes.max, :<=, Post.count # sanity check
+
+    set_sizes.each{|set_size| yield set_size }
+  end
+
+  def get_page_sizes(set_size, accumulative: false)
     max_id = Post.order(:id).limit(set_size).pluck(:id).last || -1
     page_sizes = []
 
@@ -45,13 +65,13 @@ class ControllerTest < ActionDispatch::IntegrationTest
       get posts_path, params: {
         max_id: max_id,
         page: (page_sizes.length + 1 unless page_sizes.empty?),
+        page_acc: accumulative.presence,
       }.compact
 
       page_sizes << css_select("main").text.to_i
     end
-    page_sizes.pop unless page_sizes.length == 1
 
-    page_sizes
+    page_sizes[0...-1]
   end
 
 end
